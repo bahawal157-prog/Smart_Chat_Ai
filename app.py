67862c58c1177d55f543cdec94e418d5ec1cd918
bahawal157-prog <bahawal157@gmail.com>
2026-03-24 23:39:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import google.generativeai as genai
 # یہاں اپنی Gemini API Key پیسٹ کریں
 genai.configure(api_key="AIzaSyAcXc4GQ0FIV9a3WwLpqpnSU1Uq3a-JINw")
 st.set_page_config(page_title="SmartChat AI", page_icon="🤖")
-st.title("🤖 SmartChat AI")
-st.write("السلام علیکم جانی! میں آپ کا اپنا اے آئی اسسٹنٹ ہوں۔")
+st.title("🤖 SmartChat AI"):
+st.write("السلام علیکم! میں SmartChat AI ہوں۔ بتائیے، میں آج آپ کی کیا مدد کر سکتا ہوں؟")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
